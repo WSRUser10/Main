0361a3d0f055da195e93527c4b438f6b02c09f04
WSR5 <WSRUser10@gmail.com>
2024-03-26 11:26:18 +0200

    vvs

---
+++ app/src/main/java/com/example/champ/SplashActivity.java
@@ -0,0 +1,20 @@
+package com.example.champ;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash);
+            Intent intent = new Intent(this, Onboarding.class);
+    };
+}
